7192fa9aad2c69da80aacf75ec5b664723addfe8
Clauss, Adam S <adam.clauss@swri.org>
2021-09-08 16:29:20 -0500

    Add project files.

---
+++ otel-http/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using System.Reflection;
+using System.Threading.Tasks;
+using OpenTelemetry;
+using OpenTelemetry.Trace;
+
+namespace otel_http
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+
+            ////await client.PostAsync($"http://localhost/", new StringContent("dummy data")).ConfigureAwait(false);
+
+            var tracerProvider = Sdk.CreateTracerProviderBuilder()
+                .AddSource(Assembly.GetExecutingAssembly().GetName().Name)
+                .AddHttpClientInstrumentation()
+                .AddConsoleExporter()
+                .Build();
+
+            await client.PostAsync($"http://localhost/", new StringContent("dummy data")).ConfigureAwait(false);
+        }
+    }
+}
---
+++ otel-http/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("otel-http")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Southwest Research Institute")]
+[assembly: AssemblyProduct("otel-http")]
+[assembly: AssemblyCopyright("Copyright © Southwest Research Institute 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f48e49e9-1df2-4a04-a33e-1792debc3238")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
